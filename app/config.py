1cc9b3f66aebfb54674e73715593bb29a129e3ea
stephen22otieno <stephenombiro22@gmail.com>
2018-11-10 11:12:07 +0300

    add all modified files

--- app/config.py
+++ app/config.py
@@ -1,34 +1,6 @@
-
-class Config::
-         ''''''
+class Config:
+    '''
      General configuration parent class     
-    '''
-    pass
-
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-
-
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
-classclass  ConfigConfig::
-         ''''''
-     General configuration parent class    Gener 
     '''
     pass
 
