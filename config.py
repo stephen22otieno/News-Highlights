cc41eba475dd9dd78e03ee4d95336c62b0ac4d52
stephen22otieno <stephenombiro22@gmail.com>
2018-11-10 13:02:23 +0300

    config.py and tests/source_test.py

--- config.py
+++ config.py
@@ -1,31 +1,34 @@
-class Config:
+import os 
+
+class Config:  
+
+    NEWS_ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={}'
+    NEWS_SOURCE_API_BASE_URL ="https://newsapi.org/v2/sources?apiKey={}"
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    
+
+class ProdConfig(Config): 
     '''
-     General configuration parent class     
-    '''
-
-    NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
-    HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-
-    pass
-
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
+    Production configuration child class 
     Args:
         Config: The parent configuration class with General configuration settings
     '''
     pass
 
-
-class DevConfig(Config):
+class DevConfig(Config): 
     '''
-    Development  configuration child class
-
+    Development configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
 
-    DEBUG = True
+    DEBUG = True 
+
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+} 
